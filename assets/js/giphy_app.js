53a8a3107ad821b67e5354fd6c205316483c1bf2
Joseph Cannova <jcannova@wheelership.com>
2019-02-19 17:14:35 -0500

    Create topics arrays

---
+++ assets/js/giphy_app.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+/*---------Vars, Arrays---------*/
+
+
+/*-----TOPIC ARRAYS --------*/
+var animalFails = [
+    'Dog',
+    'Cat',
+    'Elephant',
+    'Squirrel',
+    'Horse',
+    'Penguin',
+
+];
+
+var sportFails = [
+    'Basketball',
+    'Baseball',
+    'Football',
+    'Skateboard',
+    'Water Sports',
+    'Snowboarding'
+];
+
+var epicFails = [
+    'Excercise',
+    'Ramps',
+    'Kids',
+    'Drunk',
+    'BBQ'
+];
+
+var userFails = [];
