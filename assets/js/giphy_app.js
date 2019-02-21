2a543ecc8542a09994658df1ae0676eda02425bd
Joseph Cannova <jcannova@wheelership.com>
2019-02-21 14:01:56 -0500

    Added ajax, button building

--- assets/js/giphy_app.js
+++ assets/js/giphy_app.js
@@ -1,5 +1,17 @@
 
+/*--------Functions-----------*/
+function buildPresetButtons(arrayOfItems, dataType){
+   var build = "";
 
+    $(arrayOfItems).each(function(i , val) {
+        if (arrayOfItems !== undefined) {
+            build += "<button class='btn-primary btn m-1' data-" + dataType + "='" + val + "'>" + val + "</button>";
+        }
+    });
+
+
+    $("#"+dataType).html(build);
+}
 
 
 
@@ -39,3 +51,72 @@ var epicFails = [
 ];
 
 var userFails = [];
+
+
+//Initiating the topic buttons
+buildPresetButtons(animalFails, 'animalFails');
+buildPresetButtons(sportFails, 'sportFails');
+buildPresetButtons(epicFails, 'epicFails');
+
+
+/*---------Event Listeners------*/
+
+$('button').on('click', function(evt){
+
+
+    var topicPressed = evt.currentTarget.innerHTML.toLocaleLowerCase();
+
+
+    if (topicPressed === 'submit'){
+       var userValue = $('input').val();
+       userFails.push(userValue);
+
+       $('input').val('');
+
+       buildPresetButtons(userFails, 'userFails');
+
+    }else{
+        $("#giphyPlacement").empty();
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            topicPressed + " fail&api_key=WRY8y1li18n1949dMqWmTduGQn8J8wtj&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+
+
+                var data = response.data;
+
+                $(data).each(function(i,val){
+
+                    var newDiv = $("<div>");
+                    newDiv.addClass("ext-center m-4");
+
+                    var newP = $("<p>");
+                    newP.text("Rating "+ data[i].rating.toUpperCase());
+
+                    var newImg = $("<img>");
+                    newImg.attr("src", data[i].images.fixed_height.url);
+                    newImg.addClass("rounded");
+
+                    newDiv.append(newP);
+                    newDiv.append(newImg);
+
+                    $("#giphyPlacement").prepend(newDiv);
+
+
+
+
+
+                });
+
+
+                console.log(data);
+            });
+
+    };
+
+
+
+});
